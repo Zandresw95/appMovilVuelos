576754a8475cacf91ee7f0484c24c21c37fe4ace
jhonyagus <jhonyagus@hotmail.com>
2022-07-11 13:15:14 -0500

    registrar

--- src/Registrar.js
+++ src/Registrar.js
@@ -1,14 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, Button } from '@rneui/themed';
 
 const Registrar=()=> {
     const [user, setUser] = useState("");
     const [password, setPassword] = useState("");
     return (
         <View style={styles.container}>
-            <Text>Prueba</Text>
-            <StatusBar style="auto" />
+            <Input
+                placeholder='Nombres'
+                label='Ingrese nombres'
+                type='text'
+            />
+            <Input
+                placeholder='Apellidos'
+                label='Ingrese apellidos'
+                type='text'
+            />
+            <Input
+                placeholder='Cédula'
+                label='Ingrese cédula'
+                type='number'
+            />
+            <Input placeholder="Correo" 
+                label='Ingrese correo'
+                type="email" 
+            />
+            <Input placeholder="Clave" 
+                label='Ingrese clave'
+                type="password"
+                secureTextEntry={true} 
+            />
+            <View style={styles.buttons}> 
+                <Button color="secondary">Guardar</Button>
+                <Button color="warning">Salir</Button>
+            </View>
+            
 
         </View>
     );
@@ -16,9 +44,7 @@ const Registrar=()=> {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
     },
 });
